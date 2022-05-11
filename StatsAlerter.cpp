4f71386ed0c1ac3b102c0a1b8f5e900471f8181b
ShreyasTambat <105338919+ShreyasTambat@users.noreply.github.com>
2022-05-11 20:20:25 +0530

    Update StatsAlerter.cpp

--- StatsAlerter.cpp
+++ StatsAlerter.cpp
@@ -12,16 +12,19 @@ StatsAlerter::StatsAlerter(float argMaxThreshold, std::vector<IAlerter>& argAler
 
 void StatsAlerter::checkAndAlert(std::vector<float> argStatisticsValues)
 {
-  float maximumStatisticValue = *std::max_element(argStatisticsValues.begin(), argStatisticsValues.end());
+  auto maximumStatisticValue = *std::max_element(argStatisticsValues.begin(), argStatisticsValues.end());
   
-  if(maximumStatisticValue > _MaxThreshold)
-  {
-     for(auto statisticsValuesItr = _Alerters.begin(); statisticsValuesItr  != _Alerters.end(); statisticsValuesItr++)
-     {
-       statisticsValuesItr->raiseAlert();
-      // IAlerter* l_Alerters;
-      // l_Alerters = static_cast<IAlerter*>(*indOfStatisticsValues);
-      // l_Alerters->raiseAlert();
-     }
+  if(maximumStatisticValue != argStatisticsValues.end())
+  {  
+    if(maximumStatisticValue > _MaxThreshold)
+    {
+       for(auto statisticsValuesItr = _Alerters.begin(); statisticsValuesItr  != _Alerters.end(); statisticsValuesItr++)
+       {
+         statisticsValuesItr->raiseAlert();
+        // IAlerter* l_Alerters;
+        // l_Alerters = static_cast<IAlerter*>(*indOfStatisticsValues);
+        // l_Alerters->raiseAlert();
+       }
+    }
   }
 }
