93834e57d87628424d707f4f3240edfd3eba3a1b
ShreyasTambat <105338919+ShreyasTambat@users.noreply.github.com>
2022-05-11 23:25:07 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -1,7 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 
 #include "catch.hpp"
-#include "Stats.h"
+#include "stats.h"
 #include "StatsAlerter.h"
 #include "EmailAlert.h"
 #include "LEDAlert.h"
