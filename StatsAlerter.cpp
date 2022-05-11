940a29e71be67116c5ceb53f077c8d3e196f86e4
ShreyasTambat <105338919+ShreyasTambat@users.noreply.github.com>
2022-05-11 21:03:10 +0530

    Update StatsAlerter.cpp

--- StatsAlerter.cpp
+++ StatsAlerter.cpp
@@ -10,19 +10,22 @@ StatsAlerter::StatsAlerter(float argMaxThreshold, std::vector<IAlerter*>& argAle
 
 void StatsAlerter::checkAndAlert(std::vector<float>& argStatisticsValues)
 {
-  auto maximumStatisticValue = std::max_element(argStatisticsValues.begin(), argStatisticsValues.end());
-  
-  if(maximumStatisticValue != argStatisticsValues.end())
-  {  
-    if(*maximumStatisticValue > _MaxThreshold)
-    {
-       for(auto statisticsValuesItr = _Alerters.begin(); statisticsValuesItr  != _Alerters.end(); statisticsValuesItr++)
-       {
-         statisticsValuesItr->raiseAlert();
-        // IAlerter* l_Alerters;
-        // l_Alerters = static_cast<IAlerter*>(*indOfStatisticsValues);
-        // l_Alerters->raiseAlert();
-       }
+  if(!argStatisticsValues.empty())
+  {
+    auto maximumStatisticValue = std::max_element(argStatisticsValues.begin(), argStatisticsValues.end());
+
+    if(maximumStatisticValue != argStatisticsValues.end())
+    {  
+      if(*maximumStatisticValue > _MaxThreshold)
+      {
+         for(auto statisticsValuesItr = _Alerters.begin(); statisticsValuesItr  != _Alerters.end(); statisticsValuesItr++)
+         {
+           statisticsValuesItr->raiseAlert();
+          // IAlerter* l_Alerters;
+          // l_Alerters = static_cast<IAlerter*>(*indOfStatisticsValues);
+          // l_Alerters->raiseAlert();
+         }
+      }
     }
   }
 }
