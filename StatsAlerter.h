16f0fef94d92347f7c54df9545ad9f958128c724
ShreyasTambat <105338919+ShreyasTambat@users.noreply.github.com>
2022-05-11 20:12:15 +0530

    Update StatsAlerter.h

--- StatsAlerter.h
+++ StatsAlerter.h
@@ -5,10 +5,10 @@ class StatsAlerter
 {
   public:
   
-  StatsAlerter(float argMaxThreshold, std::vector<IAlerter*> argAlerters);
+  StatsAlerter(float argMaxThreshold, std::vector<IAlerter> argAlerters);
   
   void checkAndAlert(std::vector<float> argStatisticsValues);
   
   float _MaxThreshold;
-  std::vector<IAlerter*> _Alerters;
+  std::vector<IAlerter> _Alerters;
 };
